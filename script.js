3689f300fa1fac8056757b9c6e1a0b4c82136638
CodeWithNeerajWebDev <neerajsinghrawat661@gmail.com>
2026-08-27 21:52:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculateBMI() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let bmi = weight / (height * height);
+    alert("your BMI is: " + bmi.toFixed(2));
+}
